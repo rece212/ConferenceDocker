44041c063e5e3700991e6bb2af180b5a23dfc599
rece212 <rwanvig@varsitycollege.co.za>
2025-04-03 08:34:23 +0200

    Add project files.

---
+++ ConferenceAPI/Data/Config/SessionConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using ConferenceAPI.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConferenceAPI.Data.Config
+{
+    public class SessionConfiguration : IEntityTypeConfiguration<Session>
+    {
+        public void Configure(EntityTypeBuilder<Session> builder)
+        {
+            builder.HasKey(s => s.SessionID);
+
+            builder.Property(s => s.SessionTitle)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(s => s.Speaker)
+                .HasMaxLength(50);
+
+            builder.Property(s => s.SessionDateTime)
+                .IsRequired();
+
+            builder.HasData(
+                new Session
+                {
+                    SessionID = 101,
+                    SessionTitle = "Introduction to C#",
+                    Speaker = "Dr. Alan Turing",
+                    SessionDateTime = DateTime.Parse("2025-04-01T09:00:00")
+                },
+                new Session
+                {
+                    SessionID = 102,
+                    SessionTitle = "Advanced SQL Queries",
+                    Speaker = "Prof. Grace Hopper",
+                    SessionDateTime = DateTime.Parse("2025-04-01T11:00:00")
+                },
+                new Session
+                {
+                    SessionID = 103,
+                    SessionTitle = "IoT: The Future of Tech",
+                    Speaker = "Mr. Elon Mask",
+                    SessionDateTime = DateTime.Parse("2025-04-01T14:00:00")
+                }
+            );
+        }
+    }
+}
---
+++ ConferenceAPI/Models/Registration.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConferenceAPI.Models
+{
+    public class Registration
+    {
+        [Key]
+        public int RegistrationID { get; set; }
+
+        [Required]
+        [ForeignKey("Attendee")]
+        public int AttendeeID { get; set; }
+
+        [Required]
+        [ForeignKey("Session")]
+        public int SessionID { get; set; }
+
+        [Required]
+        public DateTime RegistrationDate { get; set; }
+
+        // Navigation properties for relationships
+        public Attendee Attendee { get; set; }
+        public Session Session { get; set; }
+    }
+}
